266b69c1a6727db05d1171184f818510b9134131
elkinny <vichkary@gmail.com>
2018-12-22 18:39:40 +0300

    build 1

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-document.querySelector('.burger__button').addEventListener('click', function(e) {
-  var burgerList = e.currentTarget.parentNode.querySelector('.burger__list');
-  e.currentTarget.classList.toggle('active');
-  if (burgerList.classList.length != 2) burgerList.classList.toggle('in-active');
-  burgerList.classList.toggle('active');
-});
